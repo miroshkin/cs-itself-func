a02e29a32d12d07d29c3e06e29021e327571bd39
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-10-25 12:16:18 +0300

    Added examples

--- CsItselfFunc/CsItselfFunc/Program.cs
+++ CsItselfFunc/CsItselfFunc/Program.cs
@@ -8,8 +8,45 @@ namespace CsItselfFunc
 {
     class Program
     {
+        public delegate int SomeOperation(int i, int j);
+
+        //Func is built-in delegate type.
+        //Func delegate type must return a value.
+        //Func delegate type can have zero to 16 input parameters.
+        //Func delegate does not allow ref and out parameters.
+        //Func delegate type can be used with an anonymous method or lambda expression.
+
         static void Main(string[] args)
         {
+            SomeOperation add = Sum;
+
+            int result = add(2, 3);
+            Console.WriteLine(result);
+
+            Func<int, int, int> add_v2 = Sum;
+            int result_2 = add_v2(10, 20);
+            Console.WriteLine(result_2);
+
+            Func<int> getRandomNumber = delegate()
+            {
+                Random rnd = new Random();
+                return rnd.Next(1, 100);
+            };
+            Console.WriteLine(getRandomNumber.Invoke());
+
+
+
+            Func<int> getRandomNumber_2 = () => new Random().Next(1,100);
+            Console.WriteLine(getRandomNumber_2.Invoke());
+
+            Func<int, int, int> sum = (x, y) => x + y;
+            Console.WriteLine(sum.Invoke(200,300));
+
+        }
+
+        static int Sum(int x, int y)
+        {
+            return x + y;
         }
     }
 }
